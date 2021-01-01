3453077c9c31f5f69f75edca58f116d0e3fad60d
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-01 20:49:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,5 +4,8 @@ def join_nested_strings(src)
   while row_index < mixed_data.count do 
     element_index 0
     while element_index < mixed_data[row_index].count do
-      total 
+      total += mixed_data[row_index][element_index]
+      element_index += 1
+    end
+    row_index += 1
 end
